491eeee5b93c476caf65813063aa8a8040771491
Witold Wasiczko <witold.wasiczko@rst.com.pl>
2016-03-17 13:00:00 +0100

    Move config array to separate file

--- src/ConfigProvider.php
+++ src/ConfigProvider.php
@@ -6,23 +6,6 @@ final class ConfigProvider
 {
     public function __invoke()
     {
-        return [
-            'rstgroup' => [
-                'diagnostics-middleware' => [
-                    'checks' => [],
-                ],
-            ],
-            'dependencies' => [
-                'factories' => [
-                    DiagnosticsListMiddleware::class => DiagnosticsListMiddlewareFactory::class,
-                    DiagnosticsListMiddlewareFactory::LIST_SERVICE_NAME => DiagnosticsListFactory::class,
-                    RunnerFactory::CHECKS_SERVICE_NAME => RunnerChecksFactory::class,
-                    ZendDiagnostics\Runner\Runner::class => RunnerFactory::class,
-                ],
-                'invokables' => [
-                    ResultResponseFactory\JsonResultResponseFactory::class => ResultResponseFactory\JsonResultResponseFactory::class,
-                ],
-            ],
-        ];
+        return require __DIR__ .'/../config/config.php';
     }
 }
